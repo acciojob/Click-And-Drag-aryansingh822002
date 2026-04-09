cd2e201de23e9109e6b9d3b9c10c445b5d518634
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2026-04-09 18:29:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const slider = document.querySelector('.items');
 
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let scrollLeft = 0;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+  startX = e.clientX; // ✅ use clientX
+  scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener('mouseup', () => {
@@ -22,13 +17,18 @@ slider.addEventListener('mouseup', () => {
   slider.classList.remove('active');
 });
 
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
-  
+
   e.preventDefault();
-  
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // speed multiplier
-  
+
+  const x = e.clientX; // ✅ use clientX
+  const walk = (x - startX);
+
   slider.scrollLeft = scrollLeft - walk;
 });
